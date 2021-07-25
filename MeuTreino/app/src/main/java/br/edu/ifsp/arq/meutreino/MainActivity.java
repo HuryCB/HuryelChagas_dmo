7c8e7568d924440da82e6d480c0ea2617d738502
HuryCB <huryelc.b@gmail.com>
2021-07-25 18:14:42 -0300

    MeuTreino: finalizada versao 1

---
+++ MeuTreino/app/src/main/java/br/edu/ifsp/arq/meutreino/MainActivity.java
@@ -0,0 +1,34 @@
+package br.edu.ifsp.arq.meutreino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mPeitoButton, mAbdomenButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPeitoButton = findViewById(R.id.peitoButton);
+        mPeitoButton.setOnClickListener(this);
+        mAbdomenButton = findViewById(R.id.abdomenButton);
+        mAbdomenButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.peitoButton :
+                startActivity(new Intent(MainActivity.this, PeitoActivity.class));
+                break;
+            case R.id.abdomenButton:
+                startActivity(new Intent(MainActivity.this, AbdomenActivity.class));
+                break;
+        }
+    }
+}
---
+++ MeuTreino/app/src/main/java/br/edu/ifsp/arq/meutreino/PeitoActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.arq.meutreino;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PeitoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peito);
+
+    }
+}
